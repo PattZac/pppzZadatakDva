0c4fbf44e60d4718161997c5aed0d08f324bba19
Pat <113007904+PattZac@users.noreply.github.com>
2022-12-04 19:17:26 +0100

    Add files via upload

---
+++ pppzZadatakDva/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pppzZadatakDva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prijestupne godine od 1900. do 2100. su:");
+            for (int i = 1900; i <= 2100; i++)
+            {
+                if (i % 400 == 0 || i % 4 == 0)
+                {
+                    if (i % 100 != 0) {
+                        Console.WriteLine(i+" ");
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
